ee313dc846be4e01530ef2109e265c79345b2b82
arcade15xyz <90238231+arcade15xyz@users.noreply.github.com>
2025-01-20 10:29:39 +0530

    worked with multiple slices namely 'counterSlice' and 'authSlice' then there is change in 'store' in it reducer we use  object with multiple slices reducer then export action for all slices

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,9 @@
 
 import { createSlice, configureStore } from '@reduxjs/toolkit';
 
-const initialState = {
+const initialCounterState = {
   counter: 0,
-  showCounter: true
+  showCounter: true,
 };
 
 const counterSlice = createSlice({
@@ -25,13 +25,32 @@ const counterSlice = createSlice({
   }
 });
 
+const initailAuthState = {
+  isAuthenticated: false
+}
+
+const authSlice = createSlice({
+  name: 'authentication',
+  initialState: initailAuthState ,
+
+  reducers: {
+    login(state) {
+      state.isAuthenticated = true;
+    },
+    logout(state) {
+      state.isAuthenticated = false;
+    }
+  }
+})
+
 
 
 
 const store = configureStore({
-  reducer: counterSlice.reducer
+  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer}
 });
 
 export const counterActions = counterSlice.actions;
+export const authActions = authSlice.action;
 
 export default store;
