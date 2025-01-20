2e87d568cabd39a7b3c41cf128d4e21ac7b96119
arcade15xyz <90238231+arcade15xyz@users.noreply.github.com>
2025-01-20 11:43:42 +0530

    code splitting (store index ) into 'auth.js; for  'authSlice' and 'counter.js' for 'counterSlice' and also export the actions form same file but state from same old file

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -2,7 +2,7 @@
 import { useSelector, useDispatch } from 'react-redux';
 // import { INCREMENT , INCREASE, TOGGLE, DECREMENT } from '../store/index';
 import classes from './Counter.module.css';
-import { counterActions } from '../store/index';
+import { counterActions } from '../store/counter';
 
 const Counter = () => {
   const dispatch = useDispatch();
